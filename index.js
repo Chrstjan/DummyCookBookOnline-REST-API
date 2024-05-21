ef633001069d1b77b12dd990c62048012d39b682
Christian <ps5christian@hotmail.com>
2024-05-21 17:00:13 +0200

    Working express server with .env port.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { dotenv } from "dotenv";
+import dotenv from "dotenv";
 
 const app = express();
 
 dotenv.config();
 
 const port = process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Welcome to the DummyCookBookOnline REST API");
